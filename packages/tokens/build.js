ef75764ccaa0532bd62a31383aecc49bf03002c1
Alexey Shilyaev <17354449+alexeyshilyaev@users.noreply.github.com>
2022-10-16 12:23:33 +0300

    feat(tokens): update build for tokens
    use css variables; add js tokens

--- packages/tokens/build.js
+++ packages/tokens/build.js
@@ -1,16 +1,34 @@
-import StyleDictionary from "style-dictionary";
-StyleDictionary.extend({
-  source: ['./src/**/*.json'],
-  platforms: {
-    scss: {
-      transformGroup: 'scss',
-      buildPath: 'lib/',
-      files: [{
-        destination: 'variables.scss',
-        format: 'scss/variables'
-      }]
+require("style-dictionary").extend({
+    source: ['./src/**/*.json'],
+    platforms: {
+        scss: {
+            transformGroup: 'scss',
+            buildPath: 'lib/',
+            files: [
+                {
+                    destination: 'index.css',
+                    format: 'css/variables'
+                }
+            ]
+        },
+        js: {
+            transformGroup: "js",
+            transforms: ["name/cti/camel"],
+            buildPath: 'lib/',
+            files: [
+                {
+                    format: "javascript/es6",
+                    destination: "index.js"
+                },
+                {
+                    format: "javascript/module-flat",
+                    destination: "index.module.js"
+                },
+                {
+                    format: "typescript/es6-declarations",
+                    destination: "index.d.ts"
+                }
+            ]
+        },
     }
-  }
-});
-
-StyleDictionary.buildAllPlatforms();
+}).buildAllPlatforms();
